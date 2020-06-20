c7f4e09640a9dd51fb8ec36601683ed145ce593c
Gerade Geldenhuys <GeradeGeldenhuys@gmail.com>
2020-06-20 20:31:20 +0200

    -updated and added logging

--- src/Program.cs
+++ src/Program.cs
@@ -28,7 +28,6 @@ namespace MedPark.MedicalPractice
                     webBuilder.UseContentRoot(Directory.GetCurrentDirectory())
                     .UseIISIntegration()
                     .UseStartup<Startup>();
-                });
-                //.UseLogging();
+                }).UseLogging();
     }
 }
